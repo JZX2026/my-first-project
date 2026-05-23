8b31bbe8e0c0fcc5e687e0c8f7aec61cf2250959
JZX2026 <jinzhengxuan2026@tutamail.com>
2026-05-24 04:52:07 +0800

    Create 01 (2).py

---
+++ 01 (2).py
@@ -0,0 +1,17 @@
+import turtle as t
+import time as e
+t.shape('turtle')
+t.hideturtle()
+for i in range(1000):
+    t.bgcolor('red')
+    t.bgcolor('orange')
+    t.bgcolor('yellow')
+    t.bgcolor('green')
+    t.bgcolor('sky blue')
+    t.bgcolor('blue')
+    t.bgcolor('purple')
+t.bgcolor('black')
+e.sleep(2)
+t.bgcolor('white')
+print('牛13')
+t.done()
